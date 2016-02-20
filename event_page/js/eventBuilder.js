11583fe78e1e79b80e5851513a79cd8653020d20
Qi <qizhao@Qis-MacBook-Pro.local>
2016-02-19 19:39:52 -0800

    eventBuiler modified

--- event_page/js/eventBuilder.js
+++ event_page/js/eventBuilder.js
@@ -2,4 +2,27 @@ $(document).ready(function(){
         $('.delete').on('click', function(event) {        
              $(this).parent().remove(); 
         });
-    });
+});
+
+
+$(document).ready(function(){
+        $('.edit_button').on('click', function(event) {        
+             $('.edit_text').attr("contenteditable", "true");
+        });
+});
+
+$(document).ready(function(){
+        $('.save_button').on('click', function(event) {  
+            var editElem = document.getElementsByClassName("edit_text").innerHTML;    
+            var userVersion = editElem.innerHTML;
+            localStorage.userEdits = userVersion;  
+             $('.edit_text').attr("contenteditable", "false");
+        });
+});
+
+function checkEdits() {
+
+//find out if the user has previously saved edits
+if(localStorage.userEdits!=null)
+document.getElementById("edit").innerHTML = localStorage.userEdits;
+}
