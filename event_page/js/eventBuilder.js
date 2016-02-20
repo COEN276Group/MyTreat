10df65d2493696e65b5e626225af3884ce9f4641
Qi <qizhao@Qis-MacBook-Pro.local>
2016-02-20 00:18:41 -0800

    add and delete applicant finished
    add and delete applicant finished
    dynamically happen applicant has problem with deleting. Solved the
    problem using modified o’clock function.

--- event_page/js/eventBuilder.js
+++ event_page/js/eventBuilder.js
@@ -1,9 +1,39 @@
+
+
 $(document).ready(function(){
-        $('.delete').on('click', function(event) {        
-             $(this).parent().remove(); 
+        $('.delete_app').on('click', function(event) {        
+             $(this).parent().parent().parent().hide(1000); 
         });
 });
 
+$(document).ready(function(){
+        $('.accept_app_1').on('click', function(event) { 
+             var app_div = $(this).parent().prev();
+             app_div.addClass("profile_img_div");
+             app_div.append('<input class="delete" type="button" value="X"/>'); 
+             $("#my_event_people_1").append(app_div);
+
+             $(this).parent().parent().parent().hide(1000); 
+        });
+});
+
+$(document).ready(function(){
+        $(document).on('click', ".delete", function(event) {        
+             $(this).parent().hide(800); 
+        });
+});
+
+
+$(document).ready(function(){
+        $('.accept_app_2').on('click', function(event) { 
+             var app_div = $(this).parent().prev();
+             app_div.addClass("profile_img_div");
+             app_div.append('<input class="delete" type="button" value="X"/>');  
+             $("#my_event_people_2").append(app_div);
+
+             $(this).parent().parent().parent().hide(1000); 
+        });
+});
 
 $(document).ready(function(){
         $('.edit_button').on('click', function(event) {        
