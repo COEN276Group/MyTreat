b97e91f8176a5b96137a4c8f6663c937dcae7e0d
Hairong Wang <whairong2011@gmail.com>
2016-02-20 00:20:47 -0800

    color

--- signup_page/script.js
+++ signup_page/script.js
@@ -1,5 +1,5 @@
 
-   function previewFile(){
+function previewFile(){
        var preview = document.querySelector('img'); //selects the query named img
        var file    = document.querySelector('input[type=file]').files[0]; //sames as here
        var reader  = new FileReader();
