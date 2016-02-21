3f4b6d490c6d1a8183c640be04dbcdc2a1e7d10c
Hairong Wang <whairong2011@gmail.com>
2016-02-20 16:47:02 -0800

    footer

--- signup_page/script.js
+++ signup_page/script.js
@@ -1,7 +1,7 @@
 
 function previewFile(){
-       var preview = document.querySelector('img'); //selects the query named img
-       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var preview = document.querySelector('img'); /
+       var file    = document.querySelector('input[type=file]').files[0]; 
        var reader  = new FileReader();
 
        reader.onloadend = function () {
@@ -9,10 +9,9 @@ function previewFile(){
        }
 
        if (file) {
-           reader.readAsDataURL(file); //reads the data as a URL
+           reader.readAsDataURL(file); 
        } else {
            preview.src = "";
        }
   }
 
-  previewFile();  //calls the function named previewFile()
